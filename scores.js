d87edf53349fad87bb35049e19d2442c4159b52d
jzhyang1 <jzhyang1@gmail.com>
2023-06-15 08:23:24 -0500

    WIP

---
+++ scores.js
@@ -0,0 +1,9 @@
+scoreReports = [
+  {
+    type: "score report",
+    event: "Write It Do It",
+    year: "2014-2015",
+    tournament: "Westlake Invite",
+    link: "https://drive.google.com/file/d/0By856O3q58sGTE1MNVV5bXhZZHc/view?usp=sharing&resourcekey=0-5SWBlnNkXJwaeBjBUZQ99Q",
+  },
+];
